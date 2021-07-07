a640936454f5eafeaf2c4c31a6270740d3af86df
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2021-07-07 15:57:33 +0700

    fix typo VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <v-app>
     <app-navigation/>
-    <promo />
     <v-content transition="slide-x-transition">
-      <route-view></route-view>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import AppNavigation from './components/AppNavigation.vue'
-// import Promo from './components/Promo.vue'
-// import Home from './views/Home.vue'
 export default {
   components: { AppNavigation },
 }
