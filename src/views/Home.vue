874e0ddaba749ef1ba448f59bf76bb1b0f8f4a21
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2021-07-09 00:53:22 +0700

    OrderNow Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,18 @@
     <specifications />
     <interior />
     <core-features />
+    <order-now />
   </div>
 </template>
 
 <script>
 import CoreFeatures from '../components/CoreFeatures.vue'
 import Interior from '../components/Interior.vue'
+import OrderNow from '../components/OrderNow.vue'
 import Promo from '../components/Promo.vue'
 import Specifications from '../components/Specifications.vue'
 export default {
-  components: { Promo, Specifications, Interior, CoreFeatures },
+  components: { Promo, Specifications, Interior, CoreFeatures, OrderNow },
 }
 </script>,
 
