2944b3a9a0b25d8b8321ca76ee4824093c96085c
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2021-07-09 00:36:11 +0700

    CoreFeatures, Interior Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,20 @@
   <div>
     <promo />
     <specifications />
+    <interior />
+    <core-features />
   </div>
 </template>
 
 <script>
+import CoreFeatures from '../components/CoreFeatures.vue'
+import Interior from '../components/Interior.vue'
 import Promo from '../components/Promo.vue'
 import Specifications from '../components/Specifications.vue'
 export default {
-  components: { Promo, Specifications },
+  components: { Promo, Specifications, Interior, CoreFeatures },
 }
-</script>
+</script>,
 
 <style>
 
